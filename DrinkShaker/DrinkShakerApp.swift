e93e55292e68b211f802b3ae5beecef746f04b34
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-02 09:44:36 -0400

    drink shaker initial commit

---
+++ DrinkShaker/DrinkShakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkShakerApp.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DrinkShakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
