19d844c2976189225199f61f2709f5787e01ae63
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-06 15:58:41 -0400

    added container and in memory non persistent storabe

--- DrinkShaker/DataController.swift
+++ DrinkShaker/DataController.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CoreData
+import SwiftUI
+
+class DataController: ObservableObject {
+    let container: NSPersistentCloudKitContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Main")
+        
+        // Sample data stored only in RAM
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        
+    }
+}
