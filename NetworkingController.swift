908180e38e0b06aed4935d9c879b844772440931
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-08 11:17:31 -0400

    updated fetch method with json parsing

--- NetworkingController.swift
+++ NetworkingController.swift
@@ -7,13 +7,23 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
-func fetch(_ url: URL) {
-    URLSession.shared.dataTask(with: url) { data, response, error in
-        if let error = error {
-            print("There was an error fetching data: \(error.localizedDescription)")
-        } else if let data = data {
-            // decode and print
-        }
-    }.resume()
+class NetworkingController: ObservableObject {
+    
+    func fetch(_ url: URL) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("There was an error fetching data: \(error.localizedDescription)")
+            } else if let data = data {
+                let decoder = JSONDecoder()
+                
+                do {
+                    let recipe = try decoder.decode(Recipe.self, from: data)
+                } catch  {
+                    print("The recipe call resulted in an error: \(error.localizedDescription)")
+                }
+            }
+        }.resume()
+    }
 }
