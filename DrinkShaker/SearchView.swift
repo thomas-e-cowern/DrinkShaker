e94ce872cc57de3da002f0bb18e6bbf2a98767a9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-09 10:42:23 -0400

    added search view to project and added nav view, toolbar, and search field to view

---
+++ DrinkShaker/SearchView.swift
@@ -0,0 +1,50 @@
+//
+//  SearchView.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 9/9/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    
+    @StateObject private var networkingController = NetworkingController()
+    @State var text: String = ""
+    
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle("", displayMode: .inline)
+                .toolbar {
+                    ToolbarItemGroup (placement: .primaryAction) {
+                        HStack {
+                            TextField("Search...", text: $text)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .frame(minWidth: 300)
+                                .onSubmit {
+                                    // Will be search funtion
+                                }
+                            Button {
+                                print("Search string is \(text)")
+        //                        search()
+                                
+                            } label: {
+                                Image(systemName: "magnifyingglass")
+                            }
+                            .padding(.trailing, 10)
+                        }
+                    }
+                }
+        }
+
+    }
+    
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
