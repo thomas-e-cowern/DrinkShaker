57cda3d3d83244937bd6435449b982e73948f750
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-14 15:37:10 -0400

    added random drink view

---
+++ RandomDrinkView.swift
@@ -0,0 +1,20 @@
+//
+//  RandomDrinkView.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 9/14/22.
+//
+
+import SwiftUI
+
+struct RandomDrinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RandomDrinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        RandomDrinkView()
+    }
+}
