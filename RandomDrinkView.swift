2dba46a3cc3ef44a41d566d9a0d29d2ef2adc846
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-14 16:04:11 -0400

    added formatting and hidden variable to random drink view

--- RandomDrinkView.swift
+++ RandomDrinkView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct RandomDrinkView: View {
+    
+    @StateObject private var nc = NetworkingController()
+    @State var isHidden: Bool = false
+    
     var body: some View {
         Text("Shake Your Phone for a random Drink!")
+            .opacity(isHidden ? 0 : 1)
             .onShake {
                 print("This is where we get a random recips")
+                nc.fetchRandomDrinkRecipe()
+                isHidden.toggle()
             }
     }
 }
