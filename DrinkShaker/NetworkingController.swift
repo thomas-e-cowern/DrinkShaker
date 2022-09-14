7f24778cebfb462668e1eb85c935a38ce94e5467
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-14 15:59:16 -0400

    added fetch random drink recipe method to network controller

--- DrinkShaker/NetworkingController.swift
+++ DrinkShaker/NetworkingController.swift
@@ -47,4 +47,35 @@ class NetworkingController: ObservableObject {
             }.resume()
         }
     }
+    
+    func fetchRandomDrinkRecipe() {
+        let randomDrinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+        if let url = URL(string: randomDrinkUrl) {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                DispatchQueue.main.async {
+                    if let error = error {
+                        print("There was an error with data from the drink DB: \(error.localizedDescription)")
+                    } else {
+                        let decoder = JSONDecoder()
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        
+                        if let data = data {
+                            print("Data: \(data)")
+                            if let recipes = try? decoder.decode(Recipes.self, from: data) {
+//                                print("👉 👉 👉 Recipes: \(recipes)")
+                               for recipe in recipes.drinks {
+//                                    print(recipe)
+                                   self.drinkRecipes.append(recipe)
+                                }
+//                                print("👉 👉 👉 Recipes: \(self.drinkRecipes)")
+                            } else {
+                                print("There was a problem decoding random drink: \(error?.localizedDescription ?? "Error in data")")
+                            }
+                        }
+                    }
+                }
+            }.resume()
+        }
+            
+    }
 }
