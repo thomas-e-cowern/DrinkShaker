d8c9e79f2a8e48ca4e3c9c10f603079b3bc691ab
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-14 18:08:59 -0400

    updated recipe model with ingredients

--- DrinkShaker/RecipeModel.swift
+++ DrinkShaker/RecipeModel.swift
@@ -14,6 +14,21 @@ struct RecipeModel: Codable {
     var strDrinkThumb: String?
     var strCategory: String
     var strGlass: String
+    var strIngredient1: String?
+    var strIngredient2: String?
+    var strIngredient3: String?
+    var strIngredient4: String?
+    var strIngredient5: String?
+    var strIngredient6: String?
+    var strIngredient7: String?
+    var strIngredient8: String?
+    var strIngredient9: String?
+    var strIngredient10: String?
+    var strIngredient11: String?
+    var strIngredient12: String?
+    var strIngredient13: String?
+    var strIngredient14: String?
+    var strIngredient15: String?
 }
 
 struct Recipes: Codable {
