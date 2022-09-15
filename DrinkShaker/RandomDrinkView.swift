c72823ca57813aee3fa651526337288f04ebd6e9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-15 15:06:01 -0400

    added top and bottom toolbars to random drink view

--- DrinkShaker/RandomDrinkView.swift
+++ DrinkShaker/RandomDrinkView.swift
@@ -10,70 +10,73 @@ import SwiftUI
 struct RandomDrinkView: View {
     
     @StateObject private var nc = NetworkingController()
-    @State var isShowingRecipeDetailView = false
-    @State var isHidden: Bool = false
+    @State var isHidden: Bool = true
     
     var body: some View {
-        VStack (alignment: .center) {
-            Text(nc.randomRecipe?.strDrink ?? "")
-                .font(.largeTitle)
-            HStack {
-                if nc.randomRecipe?.strDrinkThumb != nil {
-                    AsyncImage(url: URL(string:
-                        (nc.randomRecipe?.strDrinkThumb!)!)) { phase in
-                        switch phase {
-                        case .empty:
-                            ProgressView()
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(maxWidth: 100, maxHeight: 100)
-                                .padding(.leading, 5)
-                                .padding(.trailing, 5)
-                                .padding(.top, 10)
-                                .padding(.bottom, 10)
-                                .background(Color.white)
-                        case .failure(_):
-                            Image(systemName: "wind.snow")
-                                .resizable()
-                                .padding()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: 100, maxHeight: 100)
-                        @unknown default:
-                            EmptyView()
+        NavigationView {
+            VStack (alignment: .center) {
+                Text(nc.randomRecipe?.strDrink ?? "")
+                    .font(.largeTitle)
+                    .toolbar {
+                        ToolbarItem {
+                            Button {
+                                isHidden = false
+                                nc.fetchRandomDrinkRecipe()
+                            } label: {
+                                Text("Random")
+                            }
+
+                        }
+                        ToolbarItem(placement: .bottomBar) {
+                            Text("Shake Your Phone for a random Drink!")
+                        }
+                    }
+                HStack {
+                    if nc.randomRecipe?.strDrinkThumb != nil {
+                        AsyncImage(url: URL(string:
+                            (nc.randomRecipe?.strDrinkThumb!)!)) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(maxWidth: 100, maxHeight: 100)
+                                    .padding(.leading, 5)
+                                    .padding(.trailing, 5)
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 10)
+                                    .background(Color.white)
+                            case .failure(_):
+                                Image(systemName: "wind.snow")
+                                    .resizable()
+                                    .padding()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 100, maxHeight: 100)
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }
+                    }
+                }
+                .onShake {
+                    isHidden = false
+                    nc.fetchRandomDrinkRecipe()
+                }
+                Text("Served in: " + (nc.randomRecipe?.strGlass ?? ""))
+                    .font(.headline)
+                    .opacity(isHidden ? 0 : 1)
+                
+                HStack {
+                    List {
+                        ForEach(nc.randomRecipe?.ingredientsAndMeasures ?? [""], id: \.self) { ingredient in
+                            Text(ingredient)
                         }
                     }
                 }
             }
-            Text("Served in: " + (nc.randomRecipe?.strGlass ?? ""))
-                .font(.headline)
-            
-            HStack {
-                List {
-                    ForEach(nc.randomRecipe?.ingredientsAndMeasures ?? [""], id: \.self) { ingredient in
-                        Text(ingredient)
-                    }
-                }
-            }
-            
-            Button("Click for a random Drink") {
-                nc.fetchRandomDrinkRecipe()
-            }
-     
-
-            Text("Shake Your Phone for a random Drink!")
-                .opacity(isHidden ? 0 : 1)
-                .onShake {
-                    isHidden = true
-//                print("This is where we get a random recips")
-                    nc.fetchRandomDrinkRecipe()
-                    
-    //                isShowingRecipeDetailView = true
-                }
         }
-        
-
     }
 }
 
