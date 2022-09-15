90a4b569ec0a42d39bd3e00673cfa563b7b70a03
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-15 11:07:09 -0400

    updated random drink view wiht title and glass text

--- DrinkShaker/RandomDrinkView.swift
+++ DrinkShaker/RandomDrinkView.swift
@@ -15,43 +15,52 @@ struct RandomDrinkView: View {
     
     var body: some View {
         VStack {
+            HStack {
+                if nc.randomRecipe?.strDrinkThumb != nil {
+                    AsyncImage(url: URL(string: nc.drinkRecipes[0].strDrinkThumb!)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: 100, maxHeight: 100)
+                                .padding(.leading, 5)
+                                .padding(.trailing, 5)
+                                .padding(.top, 10)
+                                .padding(.bottom, 10)
+                                .background(Color.white)
+                        case .failure(_):
+    //                                    Text("Failure: \(fail.localizedDescription)")
+                            Image(systemName: "wind.snow")
+                                .resizable()
+                                .padding()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: 100, maxHeight: 100)
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                }
+                VStack {
+                    Text(nc.randomRecipe?.strDrink ?? "Default")
+                        .font(.largeTitle)
+                    Text(nc.randomRecipe?.strGlass ?? "Default")
+                        .font(.headline)
+                }
+            }
             Button("Click for a random Drink") {
                 nc.fetchRandomDrinkRecipe()
             }
      
-            if nc.randomRecipe?.strDrinkThumb != nil {
-                AsyncImage(url: URL(string: nc.drinkRecipes[0].strDrinkThumb!)) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(maxWidth: 100, maxHeight: 100)
-                            .padding(.leading, 5)
-                            .padding(.trailing, 5)
-                            .padding(.top, 10)
-                            .padding(.bottom, 10)
-                            .background(Color.white)
-                    case .failure(_):
-//                                    Text("Failure: \(fail.localizedDescription)")
-                        Image(systemName: "wind.snow")
-                            .resizable()
-                            .padding()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 100, maxHeight: 100)
-                    @unknown default:
-                        EmptyView()
-                    }
-                }
-            }
+
             Text("Shake Your Phone for a random Drink!")
                 .opacity(isHidden ? 0 : 1)
             .onShake {
                 print("This is where we get a random recips")
     //                nc.fetchRandomDrinkRecipe()
-    //                isHidden.toggle()
+                    isHidden = true
     //                isShowingRecipeDetailView = true
             }
         }
