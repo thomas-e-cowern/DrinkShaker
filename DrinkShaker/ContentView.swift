7c62b096eba501ec62337798c5d94087d2987fb8
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-13 08:09:11 -0500

    drink shaker reboot

--- DrinkShaker/ContentView.swift
+++ DrinkShaker/ContentView.swift
@@ -2,45 +2,23 @@
 //  ContentView.swift
 //  DrinkShaker
 //
-//  Created by Thomas Cowern on 9/2/22.
+//  Created by Thomas Cowern on 2/13/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house.circle")
-                    Text("Home")
-                }
-            FavoritesView()
-                .tabItem {
-                    Image(systemName: "f.circle")
-                    Text("Favorites")
-                }
-            SearchView()
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
-                }
-            RandomDrinkView()
-                .tabItem {
-                    Image(systemName: "x.circle.fill")
-                    Text("Random")
-                }
-            
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var dataController = DataController.preview
-    
-    static var previews: some View {
-        ContentView()
-            .environment(\.managedObjectContext, dataController.container.viewContext)
-            .environmentObject(dataController)
-    }
+#Preview {
+    ContentView()
 }
--- DrinkShaker/DrinkShakerApp.swift
+++ DrinkShaker/DrinkShakerApp.swift
@@ -2,25 +2,16 @@
 //  DrinkShakerApp.swift
 //  DrinkShaker
 //
-//  Created by Thomas Cowern on 9/2/22.
+//  Created by Thomas Cowern on 2/13/24.
 //
 
 import SwiftUI
 
 @main
 struct DrinkShakerApp: App {
-    @StateObject var dataController: DataController
-    
-    init() {
-        let dataContoller = DataController()
-        _dataController = StateObject(wrappedValue: dataContoller)
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, dataController.container.viewContext)
-                .environmentObject(dataController)
         }
     }
 }
