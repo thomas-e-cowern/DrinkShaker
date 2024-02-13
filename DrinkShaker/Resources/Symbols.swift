4e7a539228dad5d059eb709da5ce5c7d2044fa18
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-13 09:43:29 -0500

    add project colorsets and update symbols and theme files

--- DrinkShaker/Resources/Symbols.swift
+++ DrinkShaker/Resources/Symbols.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let wineglass = Image(systemName: "wineglass")
+    static let magnifyingglass = Image(systemName: "magnifyingglass")
+    static let heart = Image(systemName: "heart")
+}
--- DrinkShaker/Resources/Theme.swift
+++ DrinkShaker/Resources/Theme.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let carmine = Color("carmine")
+    static let eminence = Color("eminence")
+    static let dodgerBlue = Color("dodger_blue")
+}
