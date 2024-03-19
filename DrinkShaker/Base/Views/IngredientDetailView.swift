17eb016575cf5bf24da12991f2b5d12605f2e342
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-19 12:47:47 -0400

    add ingredient detail view

---
+++ DrinkShaker/Base/Views/IngredientDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientDetailView.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 3/19/24.
+//
+
+import SwiftUI
+
+struct IngredientDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientDetailView()
+}
