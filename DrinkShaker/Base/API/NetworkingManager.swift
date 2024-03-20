a960695411f296c66e12481e64de3132c8833cb3
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-20 14:34:47 -0400

    update networking manager with urlsession

--- DrinkShaker/Base/API/NetworkingManager.swift
+++ DrinkShaker/Base/API/NetworkingManager.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+final class NetworkingManager {
+    
+    static let shared = NetworkingManager()
+    
+    private init() {
+        
+    }
+    
+    func request(_ absoluteURL: String) {
+        
+        let url = URL(string: absoluteURL)
+        
+        let request = URLRequest(url: url!)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Check for errors
+            if error != nil {
+                return
+            }
+            // Check the response status code
+            guard let response = response as? HTTPURLResponse, (200...300) ~= response.statusCode else {
+                return
+            }
+            // Check if the data is nil
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+            } catch {
+                
+            }
+        }
+        
+        dataTask.resume()
+        
+    }
+}
