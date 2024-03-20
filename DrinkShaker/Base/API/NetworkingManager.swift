ce8e5c8ef218b38ee305616d6be226d9159bc99d
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-20 13:58:17 -0400

    add networking manager

---
+++ DrinkShaker/Base/API/NetworkingManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkingManager.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 3/20/24.
+//
+
+import Foundation
--- DrinkShaker/Base/Views/AlcoholDetailView.swift
+++ DrinkShaker/Base/Views/AlcoholDetailView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct AlcoholDetailView: View {
+    
+    var spirit: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            Form {
+                ScrollView {
+                    Text(spirit)
+                        .padding([.top, .bottom], 10)
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .font(.system(.body, design: .rounded))
+                        .frame(maxWidth: .infinity)
+                        .background(Theme.dodgerBlue)
+                    
+//                    if (ingredient.alcoholByVolume != nil) {
+//                        VStack(spacing: 10) {
+//                            HStack {
+//                                Text("Alcohol by Volume")
+//                                Text(ingredient.alcoholByVolume! + "%")
+//                            }
+//                            Divider()
+//                            HStack {
+//                                Text("Contains Alcohol")
+//                                Image(systemName: ingredient.ingredientContainsAlcohol == "Yes" ? "checkmark.seal.fill" : "x.circle.fill")
+//                                    .foregroundStyle(ingredient.ingredientContainsAlcohol == "Yes" ? Color.green : Color.red)
+//                            }
+//                        }
+//                        .padding(.vertical)
+//                    }
+//                    
+//                    Text(ingredient.ingredientDescrtiption)
+//                        .multilineTextAlignment(.leading)
+//                        .font(.system(.subheadline, design: .rounded).weight(.semibold))
+                    
+                    
+                }
+            }
+        }
+        .onAppear {
+            print("Appearing")
+        }
     }
 }
 
 #Preview {
-    AlcoholDetailView()
+    AlcoholDetailView(spirit: "Vodka")
 }
