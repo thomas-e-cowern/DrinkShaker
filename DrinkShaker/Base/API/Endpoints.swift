f5d11ef9499aca3f585b21c520a05b6938e9bccc
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-25 15:55:10 -0400

    add utilities to project

--- DrinkShaker/Base/API/Endpoints.swift
+++ DrinkShaker/Base/API/Endpoints.swift
@@ -15,54 +15,68 @@ enum Endpoints {
 }
 
 extension Endpoints {
-    enum MethodType {
-        case GET
-    }
-}
-
-extension Endpoints {
-    
-    // Base API address
-    var host: String { "https://www.thecocktaildb.com/api/json/v2" }
-    
-    var path: String {
+    var urlString: String {
         switch self {
         case .popularDrinks(let apiKey):
-            return "/\(apiKey)/popular.php"
+            return "https://www.thecocktaildb.com/api/json/v2/\(apiKey)/popular.php"
         case .newestDrinks(let apiKey):
-            return "/\(apiKey)/latest.php"
+            return "https://www.thecocktaildb.com/api/json/v2/\(apiKey)/latest.php"
         case .randomDrink(let apiKey):
-            return "/\(apiKey)/random.php"
+            return "https://www.thecocktaildb.com/api/json/v2/\(apiKey)/random.php"
         case .spiritDetail(let apiKey, let spiritName):
-            return "/\(apiKey)/search.php?i=\(spiritName)"
+            return  "https://www.thecocktaildb.com/api/json/v2/\(apiKey)/search.php?i=\(spiritName)"
         }
     }
 }
+//extension Endpoints {
+//    enum MethodType {
+//        case GET
+//    }
+//}
 
-extension Endpoints {
-    
-    var url: URL? {
-        var urlComponents = URLComponents()
-        
-        urlComponents.scheme = "https"
-        urlComponents.host = host
-        urlComponents.path = path
-        
-        print(urlComponents.url ?? "No URL")
-        
-        return urlComponents.url
-    }
-    
-    var methodType: MethodType {
-        switch self {
-        case .popularDrinks(let apiKey):
-                return .GET
-        case .newestDrinks(let apiKey):
-            return .GET
-        case .randomDrink(let apiKey):
-            return .GET
-        case .spiritDetail(let apiKey, let spiritName):
-            return .GET
-        }
-    }
-}
+//extension Endpoints {
+//
+//    // Base API address
+//    var host: String { "https://www.thecocktaildb.com/api/json/v2" }
+//
+//    var path: String {
+//        switch self {
+//        case .popularDrinks(let apiKey):
+//            return host + "/\(apiKey)/popular.php"
+//        case .newestDrinks(let apiKey):
+//            return "host + /\(apiKey)/latest.php"
+//        case .randomDrink(let apiKey):
+//            return host + "/\(apiKey)/random.php"
+//        case .spiritDetail(let apiKey, let spiritName):
+//            return host + "/\(apiKey)/search.php?i=\(spiritName)"
+//        }
+//    }
+//}
+
+//extension Endpoints {
+//
+//    var url: URL? {
+//        var urlComponents = URLComponents()
+//
+//        urlComponents.scheme = "https"
+//        urlComponents.host = host
+//        urlComponents.path = path
+//
+//        print(urlComponents.url ?? "No URL")
+//
+//        return urlComponents.url
+//    }
+//
+//    var methodType: MethodType {
+//        switch self {
+//        case .popularDrinks(let apiKey):
+//                return .GET
+//        case .newestDrinks(let apiKey):
+//            return .GET
+//        case .randomDrink(let apiKey):
+//            return .GET
+//        case .spiritDetail(let apiKey, let spiritName):
+//            return .GET
+//        }
+//    }
+//}
--- DrinkShaker/Resources/Constants.swift
+++ DrinkShaker/Resources/Constants.swift
@@ -10,3 +10,4 @@ import Foundation
 
 var spirits: [String] = ["Light rum", "Applejack", "Gin", "Dark rum", "Sweet Vermouth", "Strawberry schnapps", "Scotch", "Apricot brandy", "Triple sec", "Southern Comfort", "Orange bitters", "Brandy", "Lemon vodka", "Blended whiskey", "Dry Vermouth", "Amaretto", "Champagne", "Coffee liqueur", "Bourbon", "Tequila", "Vodka", "Añejo rum", "Bitters", "Kahlua", "demerara Sugar", "Dubonnet Rouge", "Irish whiskey", "Apple brandy", "Cherry brandy", "Creme de Cacao", "Port", "Coffee brandy", "Red wine", "Rum", "Ricard", "Sherry", "Cognac", "Sloe gin", "Galliano", "Peach Vodka", "Ouzo", "Spiced rum", "Angelica root", "Johnnie Walker", "Everclear", "Peach nectar", "Firewater", "Lager", "Whiskey", "Absolut Citron", "Pisco", "Irish cream", "Ale", "Chocolate liqueur", "Midori melon liqueur", "Sambuca", "Cider", "Blackberry brandy", "Peppermint schnapps", "Creme de Cassis", "Jack Daniels"]
 
+
