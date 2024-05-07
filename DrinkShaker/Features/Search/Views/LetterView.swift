2b399c660c31359f1485cd5b725dbd912cf417a4
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-07 11:06:00 -0400

    add letter view

---
+++ DrinkShaker/Features/Search/Views/LetterView.swift
@@ -0,0 +1,18 @@
+//
+//  LetterView.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 5/7/24.
+//
+
+import SwiftUI
+
+struct LetterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LetterView()
+}
