11f0921a8e493ab90a067ba079364206d46c9619
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-08 10:13:09 -0400

    update home view model to fix date issue

--- DrinkShaker/Features/Home/ViewModels/HomeViewModel.swift
+++ DrinkShaker/Features/Home/ViewModels/HomeViewModel.swift
@@ -26,9 +26,16 @@ final class HomeViewModel: ObservableObject {
             let decodedDate = customDecoder(data: data, type: Date.self)
             
             // if the date is today, return true
+            
             if Calendar.current.isDate(date, equalTo: decodedDate!, toGranularity: .day) {
+                print("This is today")
                 return true
+            } else if date > decodedDate! {
+                print("This date is younger: \(String(describing: decodedDate))")
+                return false
             }
+            
+            
         }
         
         // if not return false
@@ -55,21 +62,23 @@ final class HomeViewModel: ObservableObject {
 //            }
         } else {
 //            DispatchQueue.main.async {
-                print("has not been called today")
+                print("SOTD has not been called today")
                 // 1. check to see if there is a store SOTD
                 if UserDefaults.standard.data(forKey: "spiritOfTheDay") != nil {
                     print("there is a stored spirit of the day")
-                    // 2. if there is make sure it is displaying
-//                    DispatchQueue.main.async {
-                        if let data = UserDefaults.standard.data(forKey: "spiritOfTheDay") {
-                            
-//                            DispatchQueue.main.async {x
-                                self.spiritOfTheDay = self.customDecoder(data: data, type: Ingredient.self)
-//                            }
-                            print("stored spirit: \(String(describing: self.spiritOfTheDay))")
-                        } else {
-                            print("Decoding error SOTD: \(String(describing: self.error))")
-                        }
+                    if let spiritOfTheDayName = spirits.randomElement() {
+                        print("new spirit of the day: \(String(describing: spiritOfTheDayName))")
+                        let result = await getIngredientDetails(name: spiritOfTheDayName)
+                        //                    print("new spirit details: \(String(describing: result))")
+                        //                    spiritOfTheDay = result
+                        customEncoder(data: result, key: "spiritOfTheDay")
+                        let date = Date.now
+                        print("The new date is \(date)")
+                        customEncoder(data: Date(), key: "spiritDate")
+//                        DispatchQueue.main.async {
+                            self.spiritOfTheDay = result
+//                        }
+                    }
 //                    }
       
                     
@@ -82,6 +91,8 @@ final class HomeViewModel: ObservableObject {
                         //                    print("new spirit details: \(String(describing: result))")
                         //                    spiritOfTheDay = result
                         customEncoder(data: result, key: "spiritOfTheDay")
+                        let date = Date.now
+                        print("The new date is \(date)")
                         customEncoder(data: date, key: "spiritDate")
 //                        DispatchQueue.main.async {
                             self.spiritOfTheDay = result
@@ -102,31 +113,31 @@ final class HomeViewModel: ObservableObject {
         var storedDrinkOfTheDay: Drink?
         // 1. check to see if there is a store drink of the day
         if let drinkData = drinkData {
-            print("we have a stored DOTD")
+//            print("we have a stored DOTD")
             
             storedDrinkOfTheDay = customDecoder(data: drinkData, type: Drink.self)
             
             // 2. check to see if there is a stored date
             if let drinkDate = drinkDate {
-                print("We have a stored date")
+//                print("We have a stored date")
                 
                 // 3. decode the stored date
                 updatedDate = customDecoder(data: drinkDate, type: Date.self) ?? Date.now
                 
                 // 4.  check to see if the day is the same, if it is used the stored date
                 if Calendar.current.isDate(date, equalTo: updatedDate , toGranularity: .day) {
-                    print("Hurray, Today!")
+//                    print("Hurray, Today!")
 
                     drinkOfTheDay = storedDrinkOfTheDay
 
                     
                 } else {
-                    print("Yeah it's a new day!")
+//                    print("Yeah it's a new day!")
                     // 1. get a random drink
                     let newDrinkOfTheDay = await getRandomDrink()
                     
                     if let newDrinkOfTheDay = newDrinkOfTheDay {
-                        print("newDrinkOfTheDay: \(newDrinkOfTheDay)")
+//                        print("newDrinkOfTheDay: \(newDrinkOfTheDay)")
                     }
                     
                     drinkOfTheDay = newDrinkOfTheDay
@@ -139,9 +150,9 @@ final class HomeViewModel: ObservableObject {
                         let dateData = try encoder.encode(date)
                         
                         UserDefaults.standard.set(drinkData, forKey: "DrinkOfTheDay")
-                        print("New drink of the day should be saved")
+//                        print("New drink of the day should be saved")
                         UserDefaults.standard.set(dateData, forKey: "DrinkOfTheDayDate")
-                        print("New drink of the day date should be saved")
+//                        print("New drink of the day date should be saved")
                     } catch {
                         print("Unable to Encode DOTD (\(error))")
                     }
@@ -151,12 +162,12 @@ final class HomeViewModel: ObservableObject {
             }
             
         } else {
-            print("we do not have a stored DOTD")
+//            print("we do not have a stored DOTD")
             // 1. get a random drink
             let newDrinkOfTheDay = await getRandomDrink()
             
             if let newDrinkOfTheDay = newDrinkOfTheDay {
-                print("newDrinkOfTheDay: \(newDrinkOfTheDay)")
+//                print("newDrinkOfTheDay: \(newDrinkOfTheDay)")
             }
             // 2. save to user defaults for 24 hours
             
@@ -167,9 +178,9 @@ final class HomeViewModel: ObservableObject {
                 let dateData = try encoder.encode(date)
                 
                 UserDefaults.standard.set(drinkData, forKey: "DrinkOfTheDay")
-                print("New drink of the day should be saved")
+//                print("New drink of the day should be saved")
                 UserDefaults.standard.set(dateData, forKey: "DrinkOfTheDayDate")
-                print("New drink of the day date should be saved")
+//                print("New drink of the day date should be saved")
             } catch {
                 print("Unable to Encode DOTD (\(error))")
             }
@@ -187,7 +198,7 @@ final class HomeViewModel: ObservableObject {
     func getRandomDrink() async -> Drink? {
         // 1. Get the api key from config file
         if let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String {
-            print("Get a random drink")
+//            print("Get a random drink")
             // 2. Make the network request
             do {
                 let newDrink = try await NetworkingManager.shared.request(.randomDrink(apiKey: apiKey), type: CocktailDBAPIResponse.self).drinks.first!
@@ -229,7 +240,7 @@ final class HomeViewModel: ObservableObject {
         
         do {
             result = try decoder.decode(T.self, from: data)
-            print("Result from customDecoder: \(result)")
+//            print("Result from customDecoder: \(result)")
             return result
         } catch {
             print("Error in customDecoder; \(error)")
