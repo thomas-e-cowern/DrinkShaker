c07e2f49f644d630dab4d479f273599055d33500
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-09 12:32:16 -0400

    add extensions folder and UIDevice extension

---
+++ DrinkShaker/Extensions/UIDevice+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIDevice+Extensions.swift
+//  DrinkShaker
+//
+//  Created by Thomas Cowern on 5/9/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIDevice {
+    static let deviceDidShakeNotification = Notification.Name("deviceDidShakeNotification")
+}
